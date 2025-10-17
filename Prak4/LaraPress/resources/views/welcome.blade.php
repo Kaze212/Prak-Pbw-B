34cd69a6f9080da4ce90e981c6cddcf422bb21d7
Kaze212 <sszidanss10@gmail.com>
2025-10-17 22:13:00 +0700

    yayaya

---
+++ Prak4/LaraPress/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Selamat Datang di LaraPress</title>
+</head>
+<body>
+ <h1>Selamat Datang di Blog LaraPress</h1>
+ <p>Ini adalah halaman utama dari aplikasi blog kita.</p>
+ <a href="/tentang-kami">Lihat Halaman Tentang Kami</a>
+ <a href="/kontak">Kembali ke Halaman Utama</a>
+ <a href="/dashboard">Lihat dashboard</a>
+</body>
+</html>
